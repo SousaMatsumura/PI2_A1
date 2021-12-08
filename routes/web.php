94c2372f790815db05e176a0dcccbe1abe003043
gmcarelli <gmcarelli@gmail.com>
2021-12-08 20:58:34 -0300

    atualizando a view do cardapio.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\School\{
     Dashboard\DashboardController as SchoolDashboardController,
     FoodRecord\FoodRecordController as SchoolFoodRecordControllerController,
-    Consumption\ConsumptionController as SchoolConsumptionController
+    Consumption\ConsumptionController as SchoolConsumptionController,
+    Menu\MenuController as SchoolMenuControlller
 };
 use App\Http\Controllers\Secretary\{
     Dashboard\DashboardController as SecretaryDashboardController,
@@ -27,6 +28,12 @@ Route::middleware('auth')->group(function(){
         Route::get('consumo/cadastrar', [SchoolConsumptionController::class, 'create'])->name('school.consumption.create');
         Route::post('consumo/cadastrar', [SchoolConsumptionController::class, 'store'])->name('school.consumption.store');
         Route::patch('consumo/atualizar', [SchoolConsumptionController::class, 'update'])->name('school.consumption.update');
+
+        Route::get('cardapios', [SchoolMenuControlller::class, 'create'])->name('school.menu.index');
+        Route::get('cardapio', [SchoolMenuControlller::class, 'create'])->name('school.menu.create');
+        Route::post('cardapio', [SchoolMenuControlller::class, 'store'])->name('school.menu.store');
+        Route::patch('cardapio/atualizar', [SchoolMenuControlller::class, 'update'])->name('school.menu.update');
+
     });
 
     Route::middleware('institution.type:SECRETARY')->prefix('secretaria')->group(function(){
