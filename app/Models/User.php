790f77da639aac5fb45dd65d37928531951d9fb7
Tiago Villalobos <tiagolimavillalobos@gmail.com>
2021-12-14 18:37:09 -0300

    recuperação de senha

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Notifiable;
 
     protected $fillable = [
         'name',
