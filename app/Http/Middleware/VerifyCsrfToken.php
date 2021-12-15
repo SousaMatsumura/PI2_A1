a6adb8cb13291093a2bf41206a92303f12e769ec
Tiago Villalobos <tiagolimavillalobos@gmail.com>
2021-12-14 23:09:16 -0300

    resolvido problema de token no logout

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Contracts\Encryption\Encrypter;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -14,4 +16,12 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    public function __construct(Application $app, Encrypter $encrypter)
+    {
+        parent::__construct($app, $encrypter);
+        $this->except = [
+            route('logout')
+        ];
+    }
 }
